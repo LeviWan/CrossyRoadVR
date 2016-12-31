6dbab8d20a0c337f0ac01606579a09e4f44319bd
LeviWan <644240823@qq.com>
2016-12-31 19:05:46 +0800

    optimization of this game

---
+++ CrossyRoadVR/Assets/Scripts/CollisionOfVehicle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionOfVehicle : MonoBehaviour {
+
+    void OnCollisionEnter(Collision co)
+    {
+        if (co.gameObject.tag=="Vehicle")
+        {
+            GameState.instance.isGameOver = true;
+        }
+    }
+}
