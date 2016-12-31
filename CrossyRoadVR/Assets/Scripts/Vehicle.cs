d49906dba9e850aedc6dd0a83dd9ebc2a1a6c19d
LeviWan <644240823@qq.com>
2016-12-31 17:22:08 +0800

    Add a new feature of creating cars

---
+++ CrossyRoadVR/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vehicle : MonoBehaviour {
+
+    private float speed = 5.0f;
+
+    private float length;
+
+
+	// Use this for initialization
+	void Start () {
+        float lifeTime = length / speed;
+        Destroy(gameObject,lifeTime);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += Vector3.right * speed * Time.deltaTime;
+	
+	}
+
+    public void SetPath(float speed, float Length)
+    {
+        this.speed = speed;
+        this.length = Length;
+    }
+}
---
+++ CrossyRoadVR/Assets/Scripts/VehicleSpawner.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+    public GameObject[] vehiclePrefabs;
+    public float HeightOffset = 1.0f;
+    public float StartOffset = -10f;
+    public float Speed = 5.0f;
+    public float Length = 20.0f;
+    public float maxSpawnTime = 10.0f;
+	void Start () {
+        StartCoroutine(Spawn());
+	}
+
+    void CreateVehicle()
+    {
+       
+        int vehiclePrefabsIndex=Random.Range(0, vehiclePrefabs.Length);
+
+        GameObject vehicleObj = Instantiate(vehiclePrefabs[vehiclePrefabsIndex]);
+
+        vehicleObj.transform.position = GetPositionOffset();
+        vehicleObj.transform.parent = transform;
+        Vehicle vehicle = vehicleObj.GetComponent<Vehicle>();
+        vehicle.SetPath(Speed, Length);
+    }
+
+    Vector3 GetPositionOffset()
+    {
+        Vector3 PositionOffset = transform.position;
+        //高度的偏移
+        PositionOffset += HeightOffset * Vector3.up;
+        //宽度的偏移
+        PositionOffset += StartOffset * Vector3.right;
+        return PositionOffset;
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+           
+            WaitForSeconds waitTime=new WaitForSeconds(Random.Range(0.5f,maxSpawnTime));
+            yield return waitTime;
+            CreateVehicle();
+        }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
